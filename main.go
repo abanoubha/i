3003498069a9cdecceb4dda7f12967d03b523f90
Abanoub Hanna <abanoubhannaa73@gmail.com>
2023-03-31 03:06:25 +0200

    handle CLI args

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
+// i vim
+// go run . vim
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("i the installer\nUsage\ni <package-name>")
+		os.Exit(1)
+	}
+
+	if len(os.Args) > 2 {
+		fmt.Println("i the installer can not insall more than one package/app at once")
+		os.Exit(1)
+	}
+
+	fmt.Println("searching for", os.Args[1])
+	os.Exit(0)
 }
