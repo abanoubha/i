65a4e04704d98c818445e7c0e7dc984a96fe8d6c
Abanoub Hanna <abanoubhannaa73@gmail.com>
2023-03-31 03:43:38 +0200

    search for apps in homebrew

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 // i vim
@@ -18,6 +22,50 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("searching for", os.Args[1])
+	t := os.Args[1]
+
+	fmt.Println("searching for", t)
+
+	// check if the app already installed
+	path, err := exec.LookPath(t)
+	// if errors.Is(err, exec.ErrDot) {
+	// 	err = nil
+	// }
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	if errors.Is(err, exec.ErrNotFound) {
+		searchForApp(t)
+		os.Exit(0)
+	}
+	fmt.Println("the app you are looking for is already installed in this path", path)
 	os.Exit(0)
+}
+
+func searchForApp(t string) {
+	// brew
+	_, err := exec.LookPath("brew")
+	if errors.Is(err, exec.ErrNotFound) {
+		fmt.Println("HomeBrew (brew) is not installed")
+	} else {
+		cmd := exec.Command("brew", "search", t)
+		// cmd.Stdin = strings.NewReader("some input")
+		var out strings.Builder
+		cmd.Stdout = &out
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+		results := strings.Split(out.String(), "\n")
+
+		fmt.Printf("%q\n", cmd)
+		for _, r := range results {
+			if r == "" {
+				continue
+			}
+			fmt.Printf("%q\n", r)
+		}
+	}
+
+	// apt
 }
