0a4b5da438e322cac3afabca65eb92a6e671d067
Abanoub Hanna <abanoubhannaa73@gmail.com>
2024-05-28 11:39:44 +0300

    feature: +1 more subcommand

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
 	"os"
-	"os/exec"
-	"strings"
 )
 
 const version = "0.1.0"
@@ -25,56 +21,55 @@ func main() {
 		case "help", "--help", "-h":
 			fmt.Printf("i the abstraction over all package managers.\nUsage:\n  i install vim\n  i info vim\n  i search vim\n  i uninstall vim")
 			return
+		case "update", "upgrade", "up":
+			fmt.Println("upgrading...")
+			return
 		default:
 			fmt.Printf("'%v' sub-command is not supported in 'i'.\ntry one of these commands:\n  i install vim\n  i info vim\n  i search vim\n  i uninstall vim", os.Args[1])
 			return
 		}
 	}
 
-	t := os.Args[1]
-
-	fmt.Println("searching for", t)
-
-	// check if the app already installed
-	path, err := exec.LookPath(t)
-	// if errors.Is(err, exec.ErrDot) {
-	// 	err = nil
+	// // check if the app already installed
+	// path, err := exec.LookPath(t)
+	// // if errors.Is(err, exec.ErrDot) {
+	// // 	err = nil
+	// // }
+	// // if err != nil {
+	// // 	log.Fatal(err)
+	// // }
+	// if errors.Is(err, exec.ErrNotFound) {
+	// 	searchForApp(t)
+	// 	os.Exit(0)
 	// }
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	if errors.Is(err, exec.ErrNotFound) {
-		searchForApp(t)
-		os.Exit(0)
-	}
-	fmt.Println("the app you are looking for is already installed in this path", path)
-	os.Exit(0)
+	// fmt.Println("the app you are looking for is already installed in this path", path)
+	// os.Exit(0)
 }
 
-func searchForApp(t string) {
-	// brew
-	_, err := exec.LookPath("brew")
-	if errors.Is(err, exec.ErrNotFound) {
-		fmt.Println("HomeBrew (brew) is not installed")
-	} else {
-		cmd := exec.Command("brew", "search", t)
-		// cmd.Stdin = strings.NewReader("some input")
-		var out strings.Builder
-		cmd.Stdout = &out
-		err := cmd.Run()
-		if err != nil {
-			log.Fatal(err)
-		}
-		results := strings.Split(out.String(), "\n")
+// func searchForApp(t string) {
+// 	// brew
+// 	_, err := exec.LookPath("brew")
+// 	if errors.Is(err, exec.ErrNotFound) {
+// 		fmt.Println("HomeBrew (brew) is not installed")
+// 	} else {
+// 		cmd := exec.Command("brew", "search", t)
+// 		// cmd.Stdin = strings.NewReader("some input")
+// 		var out strings.Builder
+// 		cmd.Stdout = &out
+// 		err := cmd.Run()
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		results := strings.Split(out.String(), "\n")
 
-		fmt.Printf("%q\n", cmd)
-		for _, r := range results {
-			if r == "" {
-				continue
-			}
-			fmt.Printf("%q\n", r)
-		}
-	}
+// 		fmt.Printf("%q\n", cmd)
+// 		for _, r := range results {
+// 			if r == "" {
+// 				continue
+// 			}
+// 			fmt.Printf("%q\n", r)
+// 		}
+// 	}
 
-	// apt
-}
+// 	// apt
+// }
